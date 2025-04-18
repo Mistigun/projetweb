a09854cf016d39d0299914a16df3c668154b30fd
Mistigun <ch.courcol@outlook.fr>
2025-04-18 15:27:41 +0200

    le site la

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 include "struct/top.php";
 ?>
 
-<main>
-  
 <div class="video-banner" id="videobanner">
   <video autoplay muted loop playsinline>
     <source src="<?= RACINE ?>VIDEO/video3.mp4" type="video/mp4">
@@ -17,15 +15,25 @@ include "struct/top.php";
   
   <div class="overlay bottom-overlay">
     <p>Les voitures de l'EFREI</p>
-    
-    
-  </div>
-</div>
+    </div>
+    </div>
+
+<main>
+  
+
+  
 
 
 <article>
-  <img src="<?= RACINE ?>IMAGE/efreit.png" alt="article">
+  <img src="<?= RACINE ?>IMAGE/sebchop1.png" alt="article">
   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur in natus nisi ducimus ad vitae, dolores voluptatibus esse alias, quo, praesentium animi eos! Repellat quaerat distinctio similique doloribus velit deserunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente doloribus, numquam, unde fuga iure quo, maxime voluptatibus ut ratione tempore minima rerum omnis et placeat commodi cupiditate dignissimos nemo? Quaerat! </p>
+  <a href="PAGES/racing.php" class="btn">En savoir plus</a>
+
+</article>
+<article>
+  <img src="<?= RACINE ?>IMAGE/countach.jpeg" alt="article">
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur in natus nisi ducimus ad vitae, dolores voluptatibus esse alias, quo, praesentium animi eos! Repellat quaerat distinctio similique doloribus velit deserunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente doloribus, numquam, unde fuga iure quo, maxime voluptatibus ut ratione tempore minima rerum omnis et placeat commodi cupiditate dignissimos nemo? Quaerat! </p>
+  <a href="PAGES/sport.php" class="btn">En savoir plus</a>
 </article>
 
 
