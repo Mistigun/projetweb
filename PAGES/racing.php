f3456b2e36d1439d868c108b31286e026b0d2926
seb899 <sebastienchenal93@gmail.com>
2025-04-18 18:36:19 +0200

    v3 seb

--- PAGES/racing.php
+++ PAGES/racing.php
@@ -3,42 +3,81 @@ include '../struct/top.php';
 ?>
 
 <main>
+    <section class="car-gallery">
+        <div class="img-row">
+            <img src="<?=RACINE?>/IMAGE/sebchop2.png" alt="voiture sebchop de face">
+            <img src="<?=RACINE?>/IMAGE/sebchop1.png" alt="voiture sebchop de côté">
+            <img src="<?=RACINE?>/IMAGE/sebchop3.png" alt="voiture sebchop intérieur">
+        </div>
+        <p class="description">
+            
+        <div>
+        <h1>Hexa-Volt</h1><br>
+        <h3>Née d’une passion brûlante pour l’automobile et l’innovation, la HexaVolt a vu le jour dans les couloirs de l’EFREI Paris, au cœur d’un campus vibrant d’idées audacieuses.
+        Un groupe d’étudiants ingénieurs, unis par l’amour des mécaniques de précision et du design futuriste, a décidé de repousser les limites de ce qui était possible.</h3><br>
+        <h2>V8 Bi-Turbo Hybride</h2><br>
 
+        <h2>Spécifications :</h2><br>
+    
+        <ul> <li>Cylindrée : 4.0L</li>
+        <li>Puissance thermique : 720 ch</li>
+        <li>Puissance électrique : 200 ch (moteur électrique monté à l’avant)</li>
+        <li>Puissance combinée : 920 ch</li>
+        <li>0-100 km/h : 2.7 secondes</li>
+        <li>Vitesse max : 360 km/h</li>
+        <li>Transmission : 8 rapports double embrayage + Vectorisation du couple</li>
+    </ul></div>
+    </section>
+    <section class="car-gallery">
+        <div class="img-row">
+            <img src="<?=RACINE?>/IMAGE/aymenechop1.png" alt="voiture aymenechop de face">
+            <img src="<?=RACINE?>/IMAGE/aymenechop2.png" alt="voiture aymenechop de coté">
+            <img src="<?=RACINE?>/IMAGE/aymenechop3.png" alt="voiture aymenechop intérieur">
+        </div>
+        <p class="description">
+            
+        <div>
+        <h1>Vortex 6r</h1><br>
+        <h3>Née de l’alliance entre audace architecturale et ingénierie de pointe, la CapHex GT incarne une nouvelle vision de la compacte sportive. Son nom fait référence au dôme emblématique du logo et à sa géométrie hexagonale, symboles d'équilibre, de puissance et de précision.</h3><br>
+        <h2>2.5L Turbo Hybride</h2><br>
 
+        <h2>Spécifications :</h2><br>
+    
+        <ul> <li>Cylindrée : 2.5L</li>
+        <li>Puissance thermique : 300 ch</li>
+        <li>Puissance électrique : 110 ch </li>
+        <li>Puissance combinée : 410 ch</li>
+        <li>0-100 km/h : 4.2 secondes</li>
+        <li>Vitesse max : 270 km/h</li>
+        <li>Transmission : Automatique Double Embrayage 8 rapports </li>
+    </ul></div>
+    </section>
+    <section class="car-gallery">
+        <div class="img-row">
+            <img src="<?=RACINE?>/IMAGE/charleschop1.png" alt="voiture charleschop de face">
+            <img src="<?=RACINE?>/IMAGE/charleschop2.png" alt="voiture charleschop de coté">
+            <img src="<?=RACINE?>/IMAGE/charleschop3.png" alt="voiture charleschop intérieur">
+        </div>
+        <p class="description">
+            
+        <div>
+        <h1>HexaF1-R E²</h1><br>
+        <h3>Née de la passion brûlante d’un groupe d’étudiants ingénieurs de l’EFREI Paris, la HexaF1-R E² est le fruit d’un rêve commun : repousser les limites de la performance en alliant technologie de pointe, design racé et esprit d’équipe.</h3><br>
+        <h2>motopropulseur hybride V6 Turbo 1.6L</h2><br>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <h2>Spécifications :</h2><br>
+    
+        <ul> <li>V6 Turbo 1.6</li>
+        <li>Puissance thermique : 550 ch</li>
+        <li>Puissance électrique : 500 ch </li>
+        <li>Puissance combinée : 1050 ch</li>
+        <li>0-100 km/h : 2.4 secondes</li>
+        <li>Vitesse max : 360km/h </li>
+        <li>Transmission : Séquentielle à 8 rapports</li>
+    </ul></div>
+    </section>
+    
 </main>
-
-
 
 <?php
 include '../struct/bot.php';
--- PAGES/sport.php
+++ PAGES/sport.php
@@ -3,6 +3,71 @@ include '../struct/top.php';
 ?>
 
  <main>
+ <section class="car-gallery">
+        <div class="img-row">
+            <img src="<?=RACINE?>/IMAGE/chiron1.png" alt="chiron de face">
+            <img src="<?=RACINE?>/IMAGE/chiron2.png" alt="chiron de coté">
+            <img src="<?=RACINE?>/IMAGE/chiron3.png" alt="chiron interieur">
+        </div>
+        <p class="description">
+            
+        <div>
+        <h1>Bugatti Chiron</h1><br>
+        <h3>La Bugatti Chiron est une hypercar du constructeur automobile français Bugatti, descendante annoncée de la Bugatti Veyron 16.4[2]. Elle tient son nom du pilote automobile monégasque Louis Chiron (1899-1979)[3]. Elle est préfigurée par le concept car Bugatti Vision Gran Turismo et inspirée par la Bugatti Type 57. La Bugatti Chiron a été produite de 2016 à 2024, à 500 exemplaires, ce qui représente une production d'environ 60 exemplaires par an.</h3><br>
+        <h2>W16</h2><br>
+
+        <h2>Spécifications :</h2><br>
+    
+        <ul> <li>Cylindrée :W16</li>
+        <li>Puissance thermique : 1500 ch</li>
+        <li>0-100 km/h : 2.3 secondes</li>
+        <li>Vitesse max : 400 km/h</li>
+        <li>Transmission : Robotisée a double embrayage à 7 rapports </li>
+    </ul></div>
+
+    <section class="car-gallery">
+        <div class="img-row">
+            <img src="<?=RACINE?>/IMAGE/F40 1.png" alt="">
+            <img src="<?=RACINE?>/IMAGE/F40 2.png" alt="">
+            <img src="<?=RACINE?>/IMAGE/F40 3.png" alt="">
+        </div>
+        <p class="description">
+            
+        <div>
+        <h1>Ferrari F40</h1><br>
+        <h3>La Ferrari F40 est une supercar du constructeur automobile italien Ferrari, produite de 1987 à 1992. Elle succède à la Ferrari 288 GTO, et est commercialisée pour fêter les quarante ans de la marque. Ultime création d'Enzo Ferrari, elle est au moment de sa sortie la voiture la plus rapide, la plus puissante et la plus chère de l'histoire de l'automobile[1].</h3><br>
+        <h2>F120D V8</h2><br>
+
+        <h2>Spécifications :</h2><br>
+    
+        <ul> <li>Cylindrée : 2.9L Biturbo</li>
+        <li>Puissance thermique : 478 ch</li>
+        <li>0-100 km/h : 3,7 secondes</li>
+        <li>Vitesse max : 325 km/h</li>
+        <li>Transmission : Manuelle à 5 rapports </li>
+    </ul></div>
+
+    <section class="car-gallery">
+        <div class="img-row">
+            <img src="<?=RACINE?>/IMAGE/countach1.png" alt="countach face">
+            <img src="<?=RACINE?>/IMAGE/countach2.png" alt="countach coté">
+            <img src="<?=RACINE?>/IMAGE/countach3.png" alt="countach intérieur">
+        </div>
+        <p class="description">
+            
+        <div>
+        <h1>Lamborghini Countach</h1><br>
+        <h3>La Lamborghini Countach est une supercar GT à moteur V12 du constructeur automobile italien Lamborghini, produite à 2 042 exemplaires entre 1974 et 1990. Elle est, après la Lamborghini Miura (1966-1973) et avant la Lamborghini Diablo (1990-2001), une des voitures de sport de série les plus rapides, performantes et emblématiques de son époque.</h3><br>
+        <h2>V12 Lamborghini</h2><br>
+
+        <h2>Spécifications :</h2><br>
+    
+        <ul> <li>Cylindrée : 5.9l</li>
+        <li>Puissance thermique : 455 ch</li>
+        <li>0-100 km/h : 4.5 secondes</li>
+        <li>Vitesse max : 306 km/h</li>
+        <li>Transmission : Manuelle à 5 rapports  </li>
+    </ul></div>
 </main>
 
 
