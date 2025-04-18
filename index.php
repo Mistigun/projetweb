44f7f66c4ed8e76c7edb340b959738f4d7e59219
Mistigun <ch.courcol@outlook.fr>
2025-04-18 14:09:29 +0200

    projet masson

--- index.php
+++ index.php
@@ -18,14 +18,11 @@ include "struct/top.php";
   <div class="overlay bottom-overlay">
     <p>Les voitures de l'EFREI</p>
     
-    7
+    
   </div>
 </div>
 
-<!-- <section style="height: 2000px;">
-  <h2>Contenu de la page</h2>
-  <p>Faites défiler pour voir la bannière disparaître.</p>
-</section> -->
+
 <article>
   <img src="<?= RACINE ?>IMAGE/efreit.png" alt="article">
   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur in natus nisi ducimus ad vitae, dolores voluptatibus esse alias, quo, praesentium animi eos! Repellat quaerat distinctio similique doloribus velit deserunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente doloribus, numquam, unde fuga iure quo, maxime voluptatibus ut ratione tempore minima rerum omnis et placeat commodi cupiditate dignissimos nemo? Quaerat! </p>
